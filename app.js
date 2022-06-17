0f82b7fe75e56a0bf3c806b66f056bfd431e52b4
Gary White <glw494@gmail.com>
2022-06-17 10:33:59 -0400

    Clean code

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const submitButton = document.getElementById('submit-button');
 const info = document.getElementById('info');
 const message = document.getElementById('message');
 
-searchInput.addEventListener("keypress", (e) => {
+searchInput.addEventListener('keypress', (e) => {
   if (e.key === 'Enter' && searchInput.value !== '') {
     e.preventDefault();
     clearContent();
     createCard();
     clearInput();
-  } else {
-    message.innerHTML = 'Please enter a city.'
+  } else if (e.key === 'Enter' && searchInput.value == '') {
+    message.innerHTML = 'Please enter a city.';
   }
-})
+});
 
 submitButton.addEventListener('click', () => {
   clearContent();
@@ -20,8 +20,8 @@ submitButton.addEventListener('click', () => {
   if(searchInput.value !== '') {
     createCard();
     clearInput();
-  } else {
-    message.innerHTML = 'Please enter a city.'
+  } else if (searchInput.value == '') {
+    message.innerHTML = 'Please enter a city.';
   }
 });
 
@@ -35,49 +35,49 @@ async function getWeather() {
     const feelsLike = weatherData.main.feels_like;
     const wind = weatherData.wind.speed;
     const name = weatherData.name;
-    
+
     return {
-      name, temp, description, feelsLike, wind,
+      name, temp, description, feelsLike, wind
     };
   } catch (error) {
-    message.innerHTML = 'Can\'t find city. Please try a different entry.'
+    message.innerHTML = 'Can\'t find city. Please try a different entry.';
   }
-};
+}
 
 function createCard() {
   getWeather().then((weather) => {
     if(weather !== undefined) {
       const card = document.createElement('div');
-      const cityPara = document.createElement("p");
-      const tempPara = document.createElement("p");
-      const descriptionPara = document.createElement("p");
-      const feelsLikePara = document.createElement("p");
-      const windPara = document.createElement("p"); 
-    
-      card.classList.add("card");
-      cityPara.classList.add("city");
-      tempPara.classList.add("temp");
-      descriptionPara.classList.add("description");
-      feelsLikePara.classList.add("feels-like");
-      windPara.classList.add("wind");
-    
+      const cityPara = document.createElement('p');
+      const tempPara = document.createElement('p');
+      const descriptionPara = document.createElement('p');
+      const feelsLikePara = document.createElement('p');
+      const windPara = document.createElement('p');
+
+      card.classList.add('card');
+      cityPara.classList.add('city');
+      tempPara.classList.add('temp');
+      descriptionPara.classList.add('description');
+      feelsLikePara.classList.add('feels-like');
+      windPara.classList.add('wind');
+
       info.appendChild(card);
       card.appendChild(cityPara);
       card.appendChild(tempPara);
       card.appendChild(descriptionPara);
       card.appendChild(feelsLikePara);
       card.appendChild(windPara);
-    
+
       cityPara.innerText = weather.name;
       tempPara.innerText = weather.temp + '\u00B0F';
       descriptionPara.innerText = weather.description;
       feelsLikePara.innerText = 'Feels like: ' + weather.feelsLike + '\u00B0F';
-      windPara.innerText = 'Wind: ' + weather.wind + "mph";
+      windPara.innerText = 'Wind: ' + weather.wind + 'mph';
     } else {
-      return
+      return;
     }
-  })
-};
+  });
+}
 
 function clearInput() {
   searchInput.value = '';
